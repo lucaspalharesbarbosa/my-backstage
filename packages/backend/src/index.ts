78eef6ceafe2158cdac2ff4aa7d21ebc72aebbd5
Lucas Palhares <lucasp.b@hotmail.com>
2026-05-12 11:03:43 -0300

    chore(dev): Postgres local via Docker e configuração da base de dados - Incluído docker-compose com PostgreSQL 16 e volume persistente - Atualizado app-config para usar pg e a base backstage_main - Atualizado backend e yarn.lock

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -29,6 +29,9 @@ backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
 // See https://backstage.io/docs/auth/guest/provider
 
+// auth github plugin
+backend.add(import('@backstage/plugin-auth-backend-module-github-provider'));
+
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(
